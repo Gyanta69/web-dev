40ad7d1e99999af114afebb268c5d6d95303f431
Pragyan Khadka <137318099+Gyanta69@users.noreply.github.com>
2025-10-25 21:29:47 +0545

    outputs in js

--- Days/D-12/script.js
+++ Days/D-12/script.js
@@ -1,3 +1,20 @@
-alert("Hello, World!");
-prompt("What is your name?");
+
 console.log("This is a log message.");
+
+
+var a = 5;
+var b = 10; // bad practice: using 'var' for variable declarations
+
+let sum = a + b; // always use 'let' or 'const' for variable declarations
+
+const pi = 3.14; // good practice: using 'const' for constants
+
+//printing different types of messages to the console
+console.log("The sum of a and b is: " + sum);
+console.info(`The value of pi is approximately: ${pi}`);
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.table({ a: a, b: b, sum: sum, pi: pi });
+
+let userResponse = prompt("This is a prompt message.");
+console.log("User response: " + userResponse);
