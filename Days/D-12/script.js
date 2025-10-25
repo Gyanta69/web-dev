5e270e2b4afe5a7741b26cfc846e11cfdcfa5446
Pragyan Khadka <137318099+Gyanta69@users.noreply.github.com>
2025-10-25 22:04:51 +0545

    templet strings

--- Days/D-12/script.js
+++ Days/D-12/script.js
@@ -1,6 +1,4 @@
-
 console.log("This is a log message.");
-
 
 var a = 5;
 var b = 10; // bad practice: using 'var' for variable declarations
@@ -16,5 +14,32 @@ console.warn("This is a warning message.");
 console.error("This is an error message.");
 console.table({ a: a, b: b, sum: sum, pi: pi });
 
-let userResponse = prompt("This is a prompt message.");
-console.log("User response: " + userResponse);
+// let userResponse = prompt("This is a prompt message.");
+// console.log("User response: " + userResponse);
+
+"This is a string literal.".slice(3, 10);
+
+// templet strings
+let a = 5;
+let b = 10;
+"pragyan 2+2"`pragyan ${2 + 2}``pragyan ${a + b}``pragyan ${a * b}``pragyan ${
+  a / b
+}``pragyan ${a - b}``pragyan ${a % b}`;
+`The sum of ${a} and ${b} is ${a + b}.`;
+`The product of ${a} and ${b} is ${a * b}.`;
+`The difference when ${b} is subtracted from ${a} is ${a - b}.`;
+`The quotient when ${a} is divided by ${b} is ${a / b}.`;
+`The remainder when ${a} is divided by ${b} is ${a % b}.`;
+
+console.log(`The sum of ${a} and ${b} is ${a + b}.`);
+console.log(`The product of ${a} and ${b} is ${a * b}.`);
+console.log(`The difference when ${b} is subtracted from ${a} is ${a - b}.`);
+console.log(`The quotient when ${a} is divided by ${b} is ${a / b}.`);
+console.log(`The remainder when ${a} is divided by ${b} is ${a % b}.`);
+
+console.log("End of script.").includes("End");
+console.log("End of script.").startsWith("This");
+
+
+// statements and expressions
+//a code that gives fixed answer
