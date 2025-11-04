db82fcaa8bcfb461e0612153348fa9598a8582ea
Pragyan Khadka <137318099+Gyanta69@users.noreply.github.com>
2025-11-04 21:49:52 +0545

    more lvl2 challanges

--- Js/level2.js
+++ Js/level2.js
@@ -1,4 +1,4 @@
-// Wap which allows only 3 entries then logs in if correrct password is entered else locks the account
+// WAP which allows only 3 entries then logs in if correrct password is entered else locks the account
 
 let attempts = 0;
 let userPassword = "pragyan";
@@ -16,3 +16,43 @@ if (attempts === 3 && userPassword !== enteredPassword) {
 }else {
     console.log("Login successful.");
 }
+
+// WAP which takes word input from user until user enters 'yes' 3 times
+
+let word = prompt("Enter a word:");
+let counter = 0;
+
+while(counter !== stop) {
+    if(word === "yes") counter++;
+    word = prompt("Enter a word:");
+}
+console.log("You entered 'yes' 3 times. Stopping the loop.");
+
+// WAP to print all multiples of 7 from 1 to 51
+for(let i = 1; i <= 51; i++) {
+    if(i%7 === 0) {
+        console.log(i);
+        
+    } 
+}
+
+// WAP to calculate sum of all odd numbers from 1 to 30
+let sum = 0;
+for (let i = 1; i<31; i++) {
+    if(i%2 !== 0) {
+        sum = 0 + i;
+    }
+}
+console.log("Sum of odd numbers from 1 to 30 is: " + sum);
+
+// WAP which keeps on asking user to enter an odd number until he enters an odd number
+let num = promptmpt("Enter a number:");
+num = Number(num);
+
+while (num%2 === 0) {
+    console.log("The number " + num + " is even. Please enter an odd number:");
+    num = prompt("Enter a number:");
+    num = Number(num);
+}
+
+//
